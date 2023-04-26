1dc0a9e278b27e827411ba37f2c9b982ae50e083
Mayara <mayaradevpro@gmail.com>
2023-04-25 23:05:21 -0300

    Feat: create Math

---
+++ Class 06 - Math/index.js
@@ -0,0 +1,18 @@
+let x = 4;
+let y = 5;
+let z = 9;
+let maximum;
+let minium;
+
+//x = Math.round(x); Arrendondar
+//x = Math.floor(y); Arrendondar para baixo
+//x = Math.ceil(x); Arrendondar para cima
+//x = Math.pow(x, 2); Elevar a algum número
+//x = Math.sqrt(x);  Raiz quadrada
+//x = Math.abs(x); Valor absoluto
+//maximum = Math.max(x,y,z); O Maior
+//minium = Math.min(x,y,z);  O Menor
+maximum = Math.max(x, y, z);
+console.log(maximum);
+minium = Math.min(x, y, z);
+console.log(resposta);
